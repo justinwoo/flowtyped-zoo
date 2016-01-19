89bb4b6229ad8704bf1c879fe45f6d8c020f6533
Justin Woo <moomoowoo@gmail.com>
2016-01-19 22:32:40 +0900

    update redux def to scope type and interface declaration

--- interfaces/redux.js
+++ interfaces/redux.js
@@ -1,11 +1,11 @@
-type redux$Reducer<T, R> = (state: T, action: R) => T;
-
-interface redux$Store<T, R> {
-  subscribe(observer: () => void): void;
-  dispatch(action: R): void;
-  getState(): T;
-};
-
 declare module 'redux' {
-  declare function createStore<T, R>(reducer: redux$Reducer<T, R>): redux$Store<T, R>;
+  declare type Reducer<T, R> = (state: T, action: R) => T;
+
+  declare interface Store<T, R> {
+    subscribe(observer: () => void): void;
+    dispatch(action: R): void;
+    getState(): T;
+  }
+
+  declare function createStore<T, R>(reducer: Reducer<T, R>): Store<T, R>;
 }
